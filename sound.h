30ca817c21bd0a3768eb8f50af9f879e17cbf904
tuanbeovn <tuanquangnguyen1710@gmail.com>
2019-04-01 09:39:41 +0300

    sound.h file added

---
+++ sound.h
@@ -0,0 +1,21 @@
+// constants definition
+#define RATE 16000 	// samples per secomnd
+// data structures
+struct WAVHDR{
+	char ChunkID[4]; // it has to be "RIFF"
+	int ChunkSize; //4 byte number
+	char Format[4]; // it has tobe "WAVE"
+
+	char Subchunk1ID[4]; //"fmt"
+	int Subchunk1Size; // PCM = 16
+	short AudioFormat; // should be 1 
+	short NumChannels; // should be 1 for mono
+	int SampleRate; //1600
+	int ByteRate;	//1600*NumChannels*BitsperSample/8
+	short BlockAlign; // NumChannels*BitsPerSample/8
+	short BitsPerSample; // in our app, 16(-f S16_LE)
+
+	char Subchunk2ID[4]; // data
+	int Subchunk2Size; // 
+};
+// function declarations
